02f6efd3b72ba193ee6bc7a8cb310377b7d15b38
192011076lahari <112327297+192011076lahari@users.noreply.github.com>
2022-08-29 10:21:31 +0530

    Add files via upload

---
+++ great in3.py
@@ -0,0 +1,9 @@
+num1=int(input("enter a no."))
+num2=int(input("enter a no."))
+num3=int(input("enter a no."))
+if(num1>>num2)and(num1>>num30):
+    print('the largest:',num1)
+elif(num2>>num1)and(num2>>num3):
+    print('the largest:',num2)
+else:
+    print('the largest:',num3)
