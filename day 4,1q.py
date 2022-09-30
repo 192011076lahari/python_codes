68aa04c88ec0ae3ea449fad90fc97506d52eb434
192011076lahari <112327297+192011076lahari@users.noreply.github.com>
2022-09-30 09:49:48 +0530

    Update day 4,1q.py

--- day 4,1q.py
+++ day 4,1q.py
@@ -1,27 +1,12 @@
-n=int(input("Enter the number of elements="))
-arr=[]
-for i in range(0,n):
-    a=input("Enter the string=")
-    arr.append(a)
-print(arr)
-ch=input("Enter the order required(ASCENDING-A/DESCENDING-D)---> ")
-if(ch=='A' or ch=='a'):
-    for i in range(0,n):
-        for j in range(i+1,n):
-            if(arr[i]>arr[j]):
-                temp=arr[i]
-                arr[i]=arr[j]
-                arr[j]=temp
-    print("THE ASCENDING ORDER")
-    for i in range(0,n):
-        print(arr[i])
-elif(ch=='D' or ch=='d'):
-    for i in range(0,n):
-        for j in range(i+1,n):
-            if(arr[i]<arr[j]):
-                temp=arr[i]
-                arr[i]=arr[j]
-                arr[j]=temp
-    print("THE DESCENDING ORDER")
-    for i in range(0,n):
-        print(arr[i])
+n = int(input())
+list=[]
+for i in range(n):
+    a= (input())
+    list.append(a)
+l1=sorted(list)
+l2= sorted(list,reverse=True)
+choice = (input("a/d"))
+if(choice=="a"):
+    print("ascending order:",l1)
+else:
+    print("descending order:",l2)
